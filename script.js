51123f891ee7f149d312bc922861b24051946549
mimika <150206116+nofilo@users.noreply.github.com>
2024-03-26 11:46:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+var currentMusic = null;//可以重新声明
+let accumulate = null; 
+let answeredQuestions = []; // 全局数组用于记录已经回答过的题目
+
+function startGame() {
+  document.getElementById("start-button-container").style.display = "none";
+  document.getElementById("container").style.display = "flex";
+}
+
+function funMusic(musicFile) {
+  const audioContext = new AudioContext();
+  const source = audioContext.createBufferSource();
+  const gainNode = audioContext.createGain(); // 用于调整音量
+  const filterNode = audioContext.createBiquadFilter(); // 创建滤波器节点
+
+  // 加载音频文件
+  fetch(musicFile)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      source.buffer = audioBuffer;
+      source.connect(gainNode);
+      gainNode.connect(filterNode); // 连接到滤波器节点
+      filterNode.connect(audioContext.destination); // 连接到音频输出
+      source.start(0); // 开始播放
+      // 设置音频播放速率为2倍
+      source.playbackRate.value = 2;
+    })
+    .catch(error => console.error('Error loading audio file:', error));
+
+  // 设置滤波器节点为高通滤波器，增强高频部分
+  filterNode.type = 'highpass'; // 设置为高通滤波器
+  filterNode.frequency.value = 2000; // 设置截止频率，调整这个值可以影响声音的尖锐度
+
+  // 5秒后停止音乐
+  setTimeout(function() {
+    source.stop();
+  }, 7000); // 5000毫秒 = 5秒
+}
+
+function playMusic(musicFile) {
+currentMusic =new Audio(musicFile);
+currentMusic.play(); 
+}
+
+function showImage(element) {
+  var imageUrl = element.src;
+  document.getElementById("expanded-image").src = imageUrl;
+  document.getElementById("overlay").style.display = "block";
+  document.querySelectorAll('.gallery-item').forEach(function(item) {
+    if (item != element.parentNode) {
+      item.style.display = 'none';
+    }
+  });
+}
+
+function checkAnswer() {
+  const doorAnswers = {
+    'http://127.0.0.1:5500/image/door1.jpg': '夜海的秘密',
+    'http://127.0.0.1:5500/image/door2.jpg': '舞厅',
+    'http://127.0.0.1:5500/image/door3.jpg': '还愿',
+    'http://127.0.0.1:5500/image/door4.jpg': '清醒记'
+  };
+  const doorMusicPaths={
+    'http://127.0.0.1:5500/image/door1.jpg': './music/夜海的秘密（阉割版）.MP3',
+    'http://127.0.0.1:5500/image/door2.jpg': './music/ダンスホール.mp3',
+    'http://127.0.0.1:5500/image/door3.jpg': './music/还愿（阉割版）.MP3',
+    'http://127.0.0.1:5500/image/door4.jpg': './music/清醒记（阉割版）.MP3'
+  }
+  var answer = document.getElementById("answer-input").value.trim();
+  var currentImageSrc = document.getElementById("expanded-image").src;
+  var correctAnswer = doorAnswers[currentImageSrc];
+  var musicPath = doorMusicPaths[currentImageSrc];
+  // console.log(musicPath)
+
+  if(accumulate===0){
+    alert('您已失去答题机会。')
+  }else{
+    if (answeredQuestions.includes(currentImageSrc)) {
+      alert("该题目已经回答过，请尝试回答其他题目。");
+      return;
+    }
+    answeredQuestions.push(currentImageSrc);
+    console.log(answeredQuestions);
+
+    if (answer === correctAnswer) {
+      // 根据回答次数更新积分
+      if (answeredQuestions.length === 1) {
+        score=1000;
+      } else if (answeredQuestions.length === 2) {
+        score=2000;
+      } else if (answeredQuestions.length === 3) {
+        score=3000;
+      } else if (answeredQuestions.length === 4) {
+        score=10000;
+      }
+      accumulate += score;
+    }
+    if(answer!==correctAnswer){
+      accumulate=0;
+    }
+
+    if (answer === correctAnswer) {
+      document.getElementById("result-message").innerText = `祝贺你！回答正确，赢得了${score}块钱！`;
+      document.getElementById("result-message").style.display = "block";
+      document.querySelector("button:nth-of-type(1)").style.display = "none";
+      document.querySelector("button:nth-of-type(2)").style.display = "block";
+      document.querySelector("button:nth-of-type(3)").style.display = "block";
+    } else {
+      document.getElementById("result-message").innerText = `不好意思，您的答案错误。正确答案是"${correctAnswer}"。您的奖金清零。`;
+      document.getElementById("result-message").style.display = "block";
+      document.querySelector("button:nth-of-type(1)").style.display = "none";
+      document.querySelector("button:nth-of-type(2)").style.display = "block";
+      document.querySelector("button:nth-of-type(3)").style.display = "block";
+    }
+
+    playMusic(musicPath)
+    }
+}
+
+
+function leave() {
+  // console.log(accumulate);
+  // console.log(currentMusic);
+  if (currentMusic !== null) {
+    currentMusic.pause();
+    currentMusic = null;
+  }
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("result-message").style.display = "none";
+  document.querySelectorAll('.gallery-item').forEach(function(item) {
+    item.style.display = 'block';
+  });
+}
+
+function claimPrize() {
+  alert(`您现在累计获奖金额是${accumulate}元`)
+}
+
+function showCongratulations() {
+  document.getElementById("container").style.display = "none";
+  document.getElementById("Congratulations-page").style.display = "flex";
+  const prizeAmountElement = document.getElementById('prize-amount');
+  if(accumulate===null){
+    accumulate=0
+  }
+  prizeAmountElement.textContent = accumulate;
+}
+
+function reset(){
+  document.getElementById("Congratulations-page").style.display = "none";
+  document.getElementById("start-button-container").style.display = "flex";
+}
